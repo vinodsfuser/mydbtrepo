d681b5f96779f3ba10ecfd5429eea127b9025b56
s05352612@gmail.com <s05352612@gmail.com>
2023-04-23 14:26:35 +0000

    my second commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,6 +1,6 @@
 
 /*
-    Welcome to your first dbt model!
+    Welcome to your first dbt model! vinod
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
